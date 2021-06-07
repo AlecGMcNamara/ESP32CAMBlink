7b87e33931e12a91390db6967e376654127aa2e8
Alec McNamara <alecgmcnamara@gmail.com>
2021-06-07 15:30:29 +0100

    Fix Serial Monitor in ini file

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,14 @@ const int ledPin = 4;
 void setup() {
   // put your setup code here, to run once:
   pinMode(ledPin,OUTPUT);
+  Serial.begin(115200);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
    digitalWrite(ledPin,HIGH);
     delay(1000);
+    Serial.println("Light on");
    digitalWrite(ledPin,LOW);
     delay(1000);
 }
